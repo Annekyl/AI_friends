cb4c0db57a2f55cce7a749a8efd293861c6592cf
Annekyl <306942709@qq.com>
2026-01-29 19:51:13 +0800

    实现数据库

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from web.models.user import UserProfile
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    raw_id_fields = ("user",)   # 逗号不能删，保证传的是列表
