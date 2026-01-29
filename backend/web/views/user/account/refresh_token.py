98caeb3697da5845e63bf17bd9a715b76e9c17df
Annekyl <306942709@qq.com>
2026-01-29 21:15:09 +0800

    实现后端

---
+++ backend/web/views/user/account/refresh_token.py
@@ -0,0 +1,39 @@
+from django.conf import settings
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+class RefreshTokenView(APIView):
+    def post(self, request):
+        try:
+            refresh_token = request.COOKIES.get('refresh_token')
+            if not refresh_token:
+                return Response({
+                    'result': 'Refresh Token不存在'
+                }, status=401)  # 必须加401
+            refresh = RefreshToken(refresh_token)  # 如果Refresh Token过期了，会返回异常
+            if settings.SIMPLE_JWT['ROTATE_REFRESH_TOKENS']:
+                refresh.set_jti()
+                response = Response({
+                    'result': 'success',
+                    'access': str(refresh.access_token)
+                })
+                response.set_cookie(
+                    key='refresh_token',
+                    value=str(refresh),
+                    httponly=True,
+                    samesite='Lax',
+                    secure=False,
+                    max_age=86400 * 7,
+                )
+                return response
+
+            return Response({
+                'result': 'success',
+                'access': str(refresh.access_token),
+            })
+        except:
+            return Response({
+                'result': 'Refresh Token过期了'
+            }, status=401)  # 必须加401
